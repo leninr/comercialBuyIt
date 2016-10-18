34ce56534c5704620e66eedce53ed98c81755745
leninr <lronquillo@udlanet.ec>
2016-10-18 11:45:41 -0500

    Login change
    Agregar barra de Login Registrar a pantalla de Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,6 +33,51 @@
 </head>
 
 <body>
+
+  <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+      <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">buyIt!</a>
+      </div>
+      <!-- /.navbar-header -->
+
+      @include('alerts.success')
+      <ul class="nav navbar-top-links navbar-right">
+          <!-- Authentication Links -->
+          @if (Auth::guest())
+              <li><a href="{{ url('/login') }}">Login</a></li>
+              <li><a href="/usuario/create">Registrar</a></li>
+          @else
+              <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                      {{ Auth::user()->name }} <span class="caret"></span>
+                  </a>
+
+                  <ul class="dropdown-menu dropdown-user">
+                      <li><a href="#"><i class="fa fa-user fa-fw"></i> Perfil</a></li>
+                      <li class="divider"></li>
+                      <li>
+                          <a href="{{ url('/logout') }}"
+                              onclick="event.preventDefault();
+                                       document.getElementById('logout-form').submit();">
+                              <i class="fa fa-sign-out fa-fw"></i>
+                               Salir
+                          </a>
+
+                          <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                              {{ csrf_field() }}
+                          </form>
+                      </li>
+                  </ul>
+              </li>
+          @endif
+      </ul>
+    </nav>
   <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
